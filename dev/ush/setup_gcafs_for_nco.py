c74e68cac8725f6f2f1a178be07f64bb1bb1b8ab
bbakernoaa <barry.baker@noaa.gov>
2026-02-11 10:31:10 -0500

    Refactor job and script mappings in setup_gcafs_for_nco.py for improved clarity and maintainability

--- dev/ush/setup_gcafs_for_nco.py
+++ dev/ush/setup_gcafs_for_nco.py
@@ -19,6 +19,54 @@ current_dir_path = os.path.dirname(os.path.abspath(__file__))
 # which is assumed to be two directories up from the current file
 global_workflow_dir = os.path.abspath(os.path.join(current_dir_path, "../.."))
 
+GCAFS_EX_SCRIPTS = {
+    "exgcafs_forecast.sh": "exglobal_forecast.sh",
+    "exgcafs_prep_emissions.py": "exglobal_prep_emissions.py",
+    "exgcafs_atmos_post_manager.sh": "exglobal_atmos_pmgr.sh",
+    "exgcafs_atmos_products.sh": "exglobal_atmos_products.sh",
+}
+
+GCDAS_EX_SCRIPTS = {
+    "exgcdas_forecast.sh": "exglobal_forecast.sh",
+    "exgcdas_prep_emissions.py": "exglobal_prep_emissions.py",
+    "exgcdas_atmos_post_manager.sh": "exglobal_atmos_pmgr.sh",
+    "exgcdas_atmos_products.sh": "exglobal_atmos_products.sh",
+    "exgcdas_atmos_initialize.py": "exglobal_offline_atmos_analysis.py",
+    "exgcdas_surface_initialize.sh": "exglobal_atmos_sfcanl.sh",
+    "exgcdas_aero_analysis_initialize.py": "exglobal_aero_analysis_initialize.py",
+    "exgcdas_aero_analysis_variational.py": "exglobal_aero_analysis_variational.py",
+    "exgcdas_aero_analysis_finalize.py": "exglobal_aero_analysis_finalize.py",
+    "exgcdas_aero_analysis_calc.sh": "exglobal_atmos_analysis_calc.sh",
+    "exgcdas_aero_analysis_stats.py": "exglobal_analysis_stats.py",
+    "exgcdas_aero_analysis_generate_bmatrix.py": "exgdas_aero_analysis_generate_bmatrix.py",
+    # exgcdas_prepare_obs is taken from ObsForge for v1, not in global-workflow, do this manually!!
+    # need to add something here for the post job once Yaping's PR is in
+}
+
+GCAFS_JOBS = {
+    "JGCAFS_FORECAST": "JGLOBAL_FORECAST",
+    "JGCAFS_PREP_EMISSIONS": "JGLOBAL_PREP_EMISSIONS",
+    "JGCAFS_ATMOS_POST_MANAGER": "JGLOBAL_ATMOS_POST_MANAGER",
+    "JGCAFS_ATMOS_PRODUCTS": "JGLOBAL_ATMOS_PRODUCTS",
+}
+
+GCDAS_JOBS = {
+    "JGCDAS_FORECAST": "JGLOBAL_FORECAST",
+    "JGCDAS_PREP_EMISSIONS": "JGLOBAL_PREP_EMISSIONS",
+    "JGCDAS_ATMOS_POST_MANAGER": "JGLOBAL_ATMOS_POST_MANAGER",
+    "JGCDAS_ATMOS_PRODUCTS": "JGLOBAL_ATMOS_PRODUCTS",
+    "JGCDAS_ATMOS_INITIALIZE": "JGLOBAL_OFFLINE_ATMOS_ANALYSIS",
+    "JGCDAS_SURFACE_INITIALIZE": "JGLOBAL_ATMOS_SFCANL",
+    "JGCDAS_AERO_ANALYSIS_INITIALIZE": "JGLOBAL_AERO_ANALYSIS_INITIALIZE",
+    "JGCDAS_AERO_ANALYSIS_VARIATIONAL": "JGLOBAL_AERO_ANALYSIS_VARIATIONAL",
+    "JGCDAS_AERO_ANALYSIS_FINALIZE": "JGLOBAL_AERO_ANALYSIS_FINALIZE",
+    "JGCDAS_AERO_ANALYSIS_CALC": "JGLOBAL_ATMOS_ANALYSIS_CALC",
+    "JGCDAS_AERO_ANALYSIS_STATS": "JGLOBAL_ANALYSIS_STATS",
+    "JGCDAS_AERO_ANALYSIS_GENERATE_BMATRIX": "JGDAS_AERO_ANALYSIS_GENERATE_BMATRIX",
+    # JGCDAS_PREPARE_OBS is taken from ObsForge for v1, not in global-workflow, do this manually!!
+    # need to add something here for the post job once Yaping's PR is in
+}
+
 
 def replace_gfs_with_gcafs(input_file):
     """
@@ -335,31 +383,8 @@ def copy_job_files(global_workflow_dir):
     list
         List of tuples containing (src_path, dest_path) for copied files
     """
-    gcafs_jobs = {
-        "JGCAFS_FORECAST": "JGLOBAL_FORECAST",
-        "JGCAFS_PREP_EMISSIONS": "JGLOBAL_PREP_EMISSIONS",
-        "JGCAFS_ATMOS_POST_MANAGER": "JGLOBAL_ATMOS_POST_MANAGER",
-        "JGCAFS_ATMOS_PRODUCTS": "JGLOBAL_ATMOS_PRODUCTS",
-    }
-    gcdas_jobs = {
-        "JGCDAS_FORECAST": "JGLOBAL_FORECAST",
-        "JGCDAS_PREP_EMISSIONS": "JGLOBAL_PREP_EMISSIONS",
-        "JGCDAS_ATMOS_POST_MANAGER": "JGLOBAL_ATMOS_POST_MANAGER",
-        "JGCDAS_ATMOS_PRODUCTS": "JGLOBAL_ATMOS_PRODUCTS",
-        "JGCDAS_ATMOS_INITIALIZE": "JGLOBAL_OFFLINE_ATMOS_ANALYSIS",
-        "JGCDAS_SURFACE_INITIALIZE": "JGLOBAL_ATMOS_SFCANL",
-        "JGCDAS_AERO_ANALYSIS_INITIALIZE": "JGLOBAL_AERO_ANALYSIS_INITIALIZE",
-        "JGCDAS_AERO_ANALYSIS_VARIATIONAL": "JGLOBAL_AERO_ANALYSIS_VARIATIONAL",
-        "JGCDAS_AERO_ANALYSIS_FINALIZE": "JGLOBAL_AERO_ANALYSIS_FINALIZE",
-        "JGCDAS_AERO_ANALYSIS_CALC": "JGLOBAL_ATMOS_ANALYSIS_CALC",
-        "JGCDAS_AERO_ANALYSIS_STATS": "JGLOBAL_ANALYSIS_STATS",
-        "JGCDAS_AERO_ANALYSIS_GENERATE_BMATRIX": "JGDAS_AERO_ANALYSIS_GENERATE_BMATRIX",
-        # JGCDAS_PREPARE_OBS is taken from ObsForge for v1, not in global-workflow, do this manually!!
-        # need to add something here for the post job once Yaping's PR is in
-    }
-
     job_file_copy_list = []
-    for dest_job, src_job in {**gcafs_jobs, **gcdas_jobs}.items():
+    for dest_job, src_job in {**GCAFS_JOBS, **GCDAS_JOBS}.items():
         src_job_path = os.path.join(global_workflow_dir, 'dev', 'jobs', src_job)
         dest_job_path = os.path.join(global_workflow_dir, 'jobs', dest_job)
         job_file_copy_list.append((src_job_path, dest_job_path))
@@ -389,35 +414,12 @@ def copy_script_files(global_workflow_dir):
     list
         List of tuples containing (src_path, dest_path) for copied files
     """
-    gcafs_ex_scripts = {
-        "exgcafs_forecast.sh": "exglobal_forecast.sh",
-        "exgcafs_prep_emissions.py": "exglobal_prep_emissions.py",
-        "exgcafs_atmos_post_manager.sh": "exglobal_atmos_pmgr.sh",
-        "exgcafs_atmos_products.sh": "exglobal_atmos_products.sh",
-    }
-    gcdas_ex_scripts = {
-        "exgcdas_forecast.sh": "exglobal_forecast.sh",
-        "exgcdas_prep_emissions.py": "exglobal_prep_emissions.py",
-        "exgcdas_atmos_post_manager.sh": "exglobal_atmos_pmgr.sh",
-        "exgcdas_atmos_products.sh": "exglobal_atmos_products.sh",
-        "exgcdas_atmos_initialize.py": "exglobal_offline_atmos_analysis.py",
-        "exgcdas_surface_initialize.sh": "exglobal_atmos_sfcanl.sh",
-        "exgcdas_aero_analysis_initialize.py": "exglobal_aero_analysis_initialize.py",
-        "exgcdas_aero_analysis_variational.py": "exglobal_aero_analysis_variational.py",
-        "exgcdas_aero_analysis_finalize.py": "exglobal_aero_analysis_finalize.py",
-        "exgcdas_aero_analysis_calc.sh": "exglobal_atmos_analysis_calc.sh",
-        "exgcdas_aero_analysis_stats.py": "exglobal_analysis_stats.py",
-        "exgcdas_aero_analysis_generate_bmatrix.py": "exgdas_aero_analysis_generate_bmatrix.py",
-        # exgcdas_prepare_obs is taken from ObsForge for v1, not in global-workflow, do this manually!!
-        # need to add something here for the post job once Yaping's PR is in
-    }
-
     # if the scripts directory exists as a symlink, remove it first
     scripts_dir = os.path.join(global_workflow_dir, 'scripts')
     if os.path.islink(scripts_dir):
         os.unlink(scripts_dir)
     ex_script_file_copy_list = []
-    for dest_script, src_script in {**gcafs_ex_scripts, **gcdas_ex_scripts}.items():
+    for dest_script, src_script in {**GCAFS_EX_SCRIPTS, **GCDAS_EX_SCRIPTS}.items():
         src_script_path = os.path.join(global_workflow_dir, 'dev', 'scripts', src_script)
         dest_script_path = os.path.join(global_workflow_dir, 'scripts', dest_script)
         ex_script_file_copy_list.append((src_script_path, dest_script_path))
@@ -548,7 +550,7 @@ def setup_gcafs_for_nco():
                 content = f.read()
 
             filename = os.path.basename(file_path)
-            script_map = gcdas_ex_scripts if filename.startswith('JGCDAS_') else gcafs_ex_scripts
+            script_map = GCDAS_EX_SCRIPTS if filename.startswith('JGCDAS_') else GCAFS_EX_SCRIPTS
             
             # Replace script references using the map
             modified_job = False
